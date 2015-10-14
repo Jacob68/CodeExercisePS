b2fda71130597261f0e82b29d7050b3df8d90b41
Jacobus LaFazia <compumeister@hotmail.com>
2015-10-13 21:39:28 -0600

    Adds simple package lists to the test helper.

--- src/com/jacob68/packageInstaller/tests/TestHelper.java
+++ src/com/jacob68/packageInstaller/tests/TestHelper.java
@@ -2,6 +2,30 @@ package com.jacob68.packageInstaller.tests;
 
 public class TestHelper {
 
+	// Simple package strings
+
+	public static String[] getValidSimpleSmall() {
+		// Output: A, B, C
+		return new String[] { "A: ", "B: A", "C: B" };
+	}
+
+	public static String[] getValidSimpleLarge() {
+		// Output: C, F, D, B, A, E, G
+		return new String[] { "A: D", "D: F", "F: C", "B: F", "C: ", "E: A",
+				"G: " };
+	}
+
+	public static String[] getCyclicSimpleSmall() {
+		return new String[] { "A: B", "B: A" };
+	}
+
+	public static String[] getCyclicSimpleLarge() {
+		return new String[] { "A: D", "D: F", "F: C", "B: F", "C: B", "E: A",
+				"G: " };
+	}
+
+	// More complex package strings
+
 	public static String[] getValidSmall() {
 		return new String[] { "KittenService: CamelCaser", "CamelCaser" };
 	}
