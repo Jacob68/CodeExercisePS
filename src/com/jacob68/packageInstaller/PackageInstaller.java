19b00780daafec6613ff2c6deb36676daa343973
Jacobus LaFazia <compumeister@hotmail.com>
2015-10-13 21:42:00 -0600

    Adds stubbed method in PackageInstaller that does the work. Required for the JUnit tests.

--- src/com/jacob68/packageInstaller/PackageInstaller.java
+++ src/com/jacob68/packageInstaller/PackageInstaller.java
@@ -5,6 +5,14 @@ public class PackageInstaller {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// TODO Get package list input
+
+		// TODO Generate output, catch cycles
+	}
+
+	public String generateInstallOrder(String[] packages) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
