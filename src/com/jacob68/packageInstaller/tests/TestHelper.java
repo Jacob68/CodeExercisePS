f9ad55917b7d2f31275359cf6a13e19dd5633e2f
Jacobus LaFazia <compumeister@hotmail.com>
2015-10-13 20:42:08 -0600

    Adds static test input  methods to TestHelper class.

--- src/com/jacob68/packageInstaller/tests/TestHelper.java
+++ src/com/jacob68/packageInstaller/tests/TestHelper.java
@@ -2,4 +2,25 @@ package com.jacob68.packageInstaller.tests;
 
 public class TestHelper {
 
+	public static String[] getValidSmall() {
+		return new String[] { "KittenService: CamelCaser", "CamelCaser" };
+	}
+
+	public static String[] getValidLarge() {
+		return new String[] { "KittenService: ", "Leetmeme: Cyberportal",
+				"Cyberportal: Ice", "CamelCaser: KittenService",
+				"Fraudstream: Leetmeme", "Ice: " };
+	}
+
+	public static String[] getCyclicSmall() {
+		return new String[] { "KittenService: CamelCaser",
+				"CamelCaser: KittenService" };
+	}
+
+	public static String[] getCyclicLarge() {
+		return new String[] { "KittenService: ", "Leetmeme: Cyberportal",
+				"Cyberportal: Ice", "CamelCaser: KittenService",
+				"Fraudstream: ", "Ice: Leetmeme" };
+	}
+
 }
