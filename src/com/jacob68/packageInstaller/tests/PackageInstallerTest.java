7da2a7dd0cd377e4b0e9049377173929cb63b04b
Jacobus LaFazia <compumeister@hotmail.com>
2015-10-14 20:25:52 -0600

    Updates the test class to properly use the PackageInstaller methods.

--- src/com/jacob68/packageInstaller/tests/PackageInstallerTest.java
+++ src/com/jacob68/packageInstaller/tests/PackageInstallerTest.java
@@ -3,36 +3,57 @@ package com.jacob68.packageInstaller.tests;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
+import com.jacob68.packageInstaller.Node;
 import com.jacob68.packageInstaller.PackageInstaller;
 
+/**
+ * Test class that runs JUnit tests on the {@linkplain PackageInstaller} class
+ * methods.
+ * 
+ * @author Jacobus LaFazia
+ */
 public class PackageInstallerTest {
 
-	// TODO maybe test correct output order?
+	// TODO Since packages can have up to 1 dependent, the input list and the
+	// resolved lists should be the same length?
+	// TODO create test for length comparison
 
 	@Test
-	public void packageListsShouldBeValidAndPass() {
-		PackageInstaller installer = new PackageInstaller();
+	public void simpleSmallListShouldBeValid() {
+		// Get input package list
+		String[] input = TestHelper.getValidSimpleSmall();
 
-		String[] input1 = TestHelper.getValidSimpleSmall();
-		assertNotNull("Package list " + input1.toString()
-				+ " should generate a Non-Null install order string",
-				installer.generateInstallOrder(input1));
+		// Convert package list to node list
+		ArrayList<Node> nodes = PackageInstaller.convertPackagesToNodes(input);
 
-		// TODO Test all other valid package lists
+		// Compute dependency list
+		ArrayList<Node> resolved = PackageInstaller.computeDependencies(nodes);
+
+		assertNotNull("Package list " + input.toString()
+				+ " should generate a Non-Null install order string", resolved);
 	}
+
+	// TODO Test all other valid package lists
 
 	@Test
-	public void packageListsShouldBeCyclicAndFail() {
-		PackageInstaller installer = new PackageInstaller();
+	public void simpleSmallListShouldBeCyclic() {
+		// Get input package list
+		String[] input = TestHelper.getCyclicSimpleSmall();
 
-		String[] input1 = TestHelper.getCyclicSimpleSmall();
-		assertNull("Package list " + input1.toString()
-				+ " is cyclic and should generate a Null response",
-				installer.generateInstallOrder(input1));
+		// Convert package list to node list
+		ArrayList<Node> nodes = PackageInstaller.convertPackagesToNodes(input);
 
-		// TODO Test all other cyclic package lists
+		// Compute dependency list
+		ArrayList<Node> resolved = PackageInstaller.computeDependencies(nodes);
+
+		assertNull("Package list " + input.toString()
+				+ " is cyclic and should generate a Null resolved list",
+				resolved);
 	}
+	// TODO Test all other cyclic package lists
 
 }
